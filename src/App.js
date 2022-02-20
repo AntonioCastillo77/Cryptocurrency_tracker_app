9b051a2f206bda1b5d58386ffb3604013fddf1c6
Antonio Castillo <antoniocastillom78@gmail.com>
2022-02-20 16:52:06 -0600

    final commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainTable from "./components/MainTable";
+import Hero from "./components/Hero";
+import "./index.css"
+import { Routes, Route} from "react-router-dom";
+import CoinDetail from "./components/CoinDetail";
+
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <Header/>
+    <Hero/>
+    <Routes>
+      <Route path="/" element={<MainTable/>}/>
+      <Route path="/coinInfo" element={<CoinDetail/>}/>
+    </Routes>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+
+function Footer() {
+  return(
+    <div>
+    <footer className="footer">
+     <div className="content has-text-centered">
+       <p>
+         <strong>Crypto Tracker</strong> by Antonio Castillo Mena.
+       </p>
+     </div>
+   </footer>
+ </div>
+  );
+}
+
+export default Footer;
